9c171fd17e3f0e743bf3199ef1cba0689f340b64
mostafa <mostafa.saad.emara@gmail.com>
2020-08-01 13:59:53 +0200

    sa

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,7 +11,16 @@ class ChatScreen extends StatelessWidget {
           itemCount: 10,
           itemBuilder: (ctx, index) => Container(
               padding: const EdgeInsets.all(8), child: Text("This Work!"))),
-      floatingActionButton: FloatingActionButton(onPressed: null),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            Firestore.instance
+                .collection("/chats/MtLH41jnmo9iKoY2I2qc/messeges")
+                .snapshots()
+                .listen((event) {
+              print(event.documents[0]["text"]);
+            });
+          }),
     );
   }
 }
